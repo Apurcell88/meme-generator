e49495e615ebd8685ae5cb9cee171ffaa6890c51
Adam Purcell <apurcell88@gmail.com>
2022-11-25 13:46:43 -0600

    changed form into a div.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,10 +1,16 @@
 const MemeForm = () => {
+  function getMeme() {
+    console.log('I was clicked!');
+  }
+
   return (
-    <form className="meme--form">
-      <input className="meme--input top-input" type="text" />
-      <input className="meme--input bottom-input" type="text" />
-      <button className="meme--submit" type='submit'>Get a new meme image</button>
-    </form>
+    <main>
+      <div className="meme--form">
+        <input className="meme--input top-input" type="text" />
+        <input className="meme--input bottom-input" type="text" />
+        <button onClick={getMeme} className="meme--submit" type='submit'>Get a new meme image</button>
+      </div>
+    </main>
   );
 }
  
