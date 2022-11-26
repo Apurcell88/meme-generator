ac9220f595a114477413e4e3d74c4541fde10e4f
Adam Purcell <apurcell88@gmail.com>
2022-11-26 15:12:55 -0600

    Added getMemeImage function. Memes displaying on page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Header from './components/Header';
-import MemeForm from './components/Meme';
+import Meme from './components/Meme';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MemeForm />
+      <Meme />
     </div>
   );
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,17 +1,40 @@
-const MemeForm = () => {
-  function getMeme() {
-    console.log('I was clicked!');
+import React from "react"
+import memesData from "../memesData";
+
+const Meme = () => {
+  const [memeImage, setMemeImage] = React.useState('');
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+
+    setMemeImage(url);
   }
 
   return (
     <main>
       <div className="meme--form">
-        <input className="meme--input top-input" type="text" />
-        <input className="meme--input bottom-input" type="text" />
-        <button onClick={getMeme} className="meme--submit" type='submit'>Get a new meme image</button>
+        <input
+          className="meme--input top-input"
+          type="text"
+          placeholder="Top text"
+        />
+        <input
+          className="meme--input bottom-input"
+          type="text"
+          placeholder="Bottom text"
+        />
+        <button
+          className="meme--submit" type='submit'
+          onClick={getMemeImage}
+        >
+          Get a new meme image
+        </button>
       </div>
+      <img className="meme--img" src={memeImage} alt="meme" />
     </main>
   );
 }
  
-export default MemeForm;
+export default Meme;
