2964e0a50dbc2c67bbb368943db73765fe47dd8e
Adam Purcell <apurcell88@gmail.com>
2022-12-07 15:24:52 -0600

    Added useEffect to fetch an API. Cleaned up getMeme function so it works with allMemes state.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,18 +1,24 @@
 /* eslint-disable no-restricted-globals */
 import React from "react"
-import memesData from "../memesData";
 
 const GenerateMeme = () => {
+
   const [meme, setMeme] = React.useState({
     topText: '',
     bottomText: '',
     randomImage: 'http://i.imgflip.com/1bij.jpg'
   });
 
-  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+  const [allMemes, setAllMemes] = React.useState([]);
 
-  function getMemeImage() {
-    const memesArray = allMemeImages.data.memes;
+  React.useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then(res => res.json())
+      .then(data => setAllMemes(data.data.memes))
+  }, [])
+
+  function getMeme() {
+    const memesArray = allMemes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
     const url = memesArray[randomNumber].url;
   
@@ -55,7 +61,7 @@ const GenerateMeme = () => {
         />
         <button
           className="meme--submit" type='submit'
-          onClick={getMemeImage}
+          onClick={getMeme}
         >
           Get a new meme image
         </button>
